e3c961a721d6c550285a86601f833da6fd21aea4
jasper-lai <jasper.lai@gmail.com>
2017-01-16 13:47:39 +0800

    [綠燈] Scenario_1_第一集買了一本_價格應為100元

--- PotterShoppingCart/ShoppingCart.cs
+++ PotterShoppingCart/ShoppingCart.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PotterShoppingCart
 {
@@ -11,7 +12,10 @@ namespace PotterShoppingCart
 
         public int CalcAmt(List<Order> orders)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            //不打折的狀況下, 直接用 Linq 處理
+            result = orders.Sum(x => x.Book.Price*x.Qty);
+            return result;
         }
     }
 
