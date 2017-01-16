78a05f826c184a88d9ffd9c12a9cf0599daa9905
jasper-lai <jasper.lai@gmail.com>
2017-01-16 13:52:35 +0800

    [紅燈] Scenario_2_第一集買了一本_第二集也買了一本_價格應為190

--- PotterShoppingCart.Tests/ShoppingCartTests.cs
+++ PotterShoppingCart.Tests/ShoppingCartTests.cs
@@ -28,6 +28,28 @@ namespace PotterShoppingCart.Tests
             Assert.AreEqual(expected, actual);
 
         }
+
+        [TestMethod]
+        public void Scenario_2_第一集買了一本_第二集也買了一本_價格應為190()
+        {
+            //arrange
+            List<Order> orders = new List<Order>()
+            {
+                new Order { Book = new Book() {VolNo = 1}, Qty = 1},
+                new Order { Book = new Book() {VolNo = 2}, Qty = 1}
+            };
+
+            //act
+            var target = new ShoppingCart();
+            int actual = target.CalcAmt(orders);
+
+            //assert
+            int expected = 190;
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
     }
 
 }
