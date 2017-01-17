1acf28a25b734e32e31e46aa8e38ffcfcd6d2a22
jasper-lai <jasper.lai@gmail.com>
2017-01-17 13:52:10 +0800

    [紅燈] (自行加入的測試案例)  Scenario_8_第一集買了一本_第二集買了兩本_第三集買了四本_價格應為660

--- PotterShoppingCart.Tests/ShoppingCartTests.cs
+++ PotterShoppingCart.Tests/ShoppingCartTests.cs
@@ -157,6 +157,35 @@ namespace PotterShoppingCart.Tests
 
         }
 
+        /// <summary>
+        /// Scenario_8_第一集買了一本_第二集買了兩本_第三集買了四本_價格應為660
+        /// </summary>
+        /// <remarks>
+        /// 自行新增的測試案例: 假設最後剩下的數量, 是同一集, 那就不打折
+        /// 例如: 第1集買了2本 ==> 100*2 = 200
+        /// 例如: 第1集買了1本, 第2集買了3本 ==> 100*2*.95 + 100*2 = 390
+        /// 例如: 第1集買了1本, 第2集買了2本, 第3集買了4本 ==> 100*3*.9 + 100*2*.95 + 100*2 = 
+        /// </remarks>
+        [TestMethod]
+        public void Scenario_8_第一集買了一本_第二集買了兩本_第三集買了四本_價格應為660()
+        {
+            //arrange
+            List<Order> orders = new List<Order>()
+            {
+                new Order { Book = new Book() {VolNo = 1}, Qty = 1},
+                new Order { Book = new Book() {VolNo = 2}, Qty = 2},
+                new Order { Book = new Book() {VolNo = 3}, Qty = 4},
+            };
+
+            //act
+            var target = new ShoppingCart();
+            int actual = target.CalcAmt(orders);
+
+            //assert
+            int expected = 660;     // 100*3*0.9 + 100*2*0.95 + 100*2*1 = 660
+            Assert.AreEqual(expected, actual);
+
+        }
 
 
     }
